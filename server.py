ffdb05712dc2ddd7a286be772377f6bb9d25c7d5
gerard-bisama <gerbis2000@gmail.com>
2026-05-20 14:41:41 -0400

    Improve code compliance with PEP8

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Executing this function initiates the application of emotion
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask,render_template,request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,7 +10,12 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-    # Retrieve the text to analyze from the request arguments 
+    ''' This code receives the text from the HTML interface and 
+        runs emotion analysis over it using emotion_detector()
+        function. The output returned shows the labels and its confidence 
+        scores for the provided text.
+    '''
+    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
@@ -19,9 +28,17 @@ def sent_analyzer():
         joy_score = response['joy']
         sadness_score = response['sadness']
         dominant_emotion = response['dominant_emotion']
-        formated_response = "For the given statement, the system response is 'anger':{}, " \
-            " 'disgust':{}, 'fear':{}, 'joy':{} and 'sadness':{}. The dominant emotion is {}." \
-            " ".format(anger_score,disgust_score,fear_score,joy_score,sadness_score,dominant_emotion)
+        #formated_response = "For the given statement, the system response is 'anger':{}, " \
+        #  " 'disgust':{}, 'fear':{}, 'joy':{} and 'sadness':{}. The dominant emotion is {}." \
+        #   " ".format(anger_score,disgust_score,fear_score,joy_score,
+        #   sadness_score,dominant_emotion)
+        formated_response_1 = "For the given statement, the system response"
+        formated_response_1 = formated_response_1 + f"is 'anger':{anger_score}, "
+        formated_response_2  = f"'disgust':{disgust_score}, 'fear':{fear_score},"
+        formated_response_2  = formated_response_2 + f"'joy':{joy_score} and "
+        formated_response_3  = f"'sadness':{sadness_score}. "
+        formated_response_3  = formated_response_3 + f"The dominant emotion is {dominant_emotion}. "
+        formated_response = formated_response_1 + formated_response_2 + formated_response_3
     return formated_response
 
 @app.route("/")
@@ -32,7 +49,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
-    #TODO
     app.run(host="0.0.0.0", port=5000)
